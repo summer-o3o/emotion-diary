377027f38105f2fa1f3d000d8e12abc9aee86764
summer-o3o <ho_ho95@naver.com>
2023-05-11 17:22:27 +0900

    add : edit페이지에 쿼리스트링 적용

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,21 @@
+import { useSearchParams } from 'react-router-dom';
+
 const Edit = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get('id');
+  console.log('id:', id);
+
+  const mode = searchParams.get('mode');
+  console.log('mode:', mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: 'sumi' })}>
+        쿼리스트링 바꾸기
+      </button>
     </div>
   );
 };
