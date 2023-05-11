7c99d07b58c8a767ce7bfd705d3d60d5bb4e5163
summer-o3o <ho_ho95@naver.com>
2023-05-11 17:26:19 +0900

    add : navigate 적용해보기 (뒤로가기)

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,6 +1,7 @@
-import { useSearchParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 const Edit = () => {
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const id = searchParams.get('id');
@@ -16,6 +17,8 @@ const Edit = () => {
       <button onClick={() => setSearchParams({ who: 'sumi' })}>
         쿼리스트링 바꾸기
       </button>
+      <button onClick={() => navigate('/')}>home으로 가기</button>
+      <button onClick={() => navigate(-1)}>뒤로가기</button>
     </div>
   );
 };
