e40268913abcc414b04465a3cd8f1a574a4e3ea0
summer-o3o <ho_ho95@naver.com>
2023-05-13 12:32:44 +0900

    add : 일기 상세 페이지 구현

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -1,55 +1,13 @@
 import React, { useContext, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { DiaryDispatchContext } from '../Router';
+
+import { getStringDate } from '../util/date';
+import { emotionList } from '../util/emotion';
+
 import MyHeader from './MyHeader';
 import MyButton from './MyButton';
 import EmotionItem from './EmotionItem';
-
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: '완전 좋음',
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_descript: '완전 좋음',
-  },
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: '그럭저럭',
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: '나쁨',
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_descript: '끔찍함',
-  },
-];
-
-export const getStringDate = date => {
-  let year = date.getFullYear();
-
-  let month = date.getMonth() + 1;
-
-  let day = date.getDate();
-
-  if (month < 10) {
-    month = `0${month}`;
-  }
-
-  if (day < 10) {
-    day = `0${day}`;
-  }
-
-  return `${year}-${month}-${day}`;
-};
 
 const DiaryEditor = ({ isEdit, originData }) => {
   const contentRef = useRef();
