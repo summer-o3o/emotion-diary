0a03bb9eb87dfcadf9fe476299d8d3167c9a73b5
summer-o3o <ho_ho95@naver.com>
2023-10-24 08:54:22 +0900

    update : 타입 스크립트 초기셋팅

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './style/common.css';
 import Router from './Router';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(<Router />);
